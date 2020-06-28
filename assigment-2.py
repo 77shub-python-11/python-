f4df2550fcc5e4779d8a31f9c33b6c04d1b43a33
77shub-python-11 <pythonmaster77gmail.com>
2020-06-28 21:24:50 +0530

    Q was not there

--- assigment-2.py
+++ assigment-2.py
@@ -1,3 +1,12 @@
+
+
+'''
+ Create a list of names dynamically as per user choice and let the user decide when he wants  to quit?
+hint:while if break,ask user wants to continue take input as yes or no -- y,n.
+'''
+
+
+
 while True:
     print("do u want make list y / n")
     i = input("only y/n: ")
