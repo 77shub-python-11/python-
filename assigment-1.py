6db9e726d81941e51e78cebe57ac9b8d3e36902b
77shub-python-11 <pythonmaster77gmail.com>
2020-06-29 16:23:31 +0530

    basic

---
+++ assigment-1.py
@@ -0,0 +1,18 @@
+a= 11.23
+b=23.2
+c=45.2
+abc=[["abc",a],
+     ["cbx",b],
+     ["xzc",c ]]
+print(abc)
+print(abc[0][0])
+it=['a','b','c','n','d','h']
+
+print(it)
+print(type(it))
+
+l=[]
+l.append("a")
+l.append("b")
+
+print(l)
--- assigment-2.py
+++ assigment-2.py
@@ -6,16 +6,30 @@ hint:while if break,ask user wants to continue take input as yes or no -- y,n.
 '''
 
 
-
+b=[]
 while True:
     print("do u want make list y / n")
     i = input("only y/n: ")
     if i == "y":
-        a = input()
-        b = []
-        b.append(a)
-        print(a)
+         a = input()
+
+         b.append(a)
+         print(a)
 
     elif i == "n":
         print("ok fine")
         break
+
+
+
+l=[]
+a=0
+while True:
+    b=input()
+    l.append(b)
+    print(l)
+    var=input("u want enter more y/n : ")
+
+
+
+
